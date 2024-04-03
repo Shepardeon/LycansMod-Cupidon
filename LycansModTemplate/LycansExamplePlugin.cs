6019380f424f1ccbcaef54a8f5a322c000b92a7b
Thomas <thomas.demaen@life-sensation.fr>
2024-04-03 18:56:40 +0200

    Initial commit

---
+++ LycansModTemplate/LycansExamplePlugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace LycansModTemplate
+{
+    // Mod d'exemple qui permet à un joueur de péter sur commande en appuyant sur la touche P
+    // de son clavier. Vous pouvez vous en servir comme point de départ pour créer vos mods sur
+    // Lycans.
+
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInProcess("Lycans.exe")]
+    public class LycansExamplePlugin : BaseUnityPlugin
+    {
+        // Le GUID du plugin doit être unique au plugin que vous créez
+        // Il doit également être lisible car est utilisé dans le nom des fichiers de config
+        // Changez le nom d'auteur et le nom du plugin afin que le GUID soit correctement
+        // initialisé pour votre mod
+        public const string PLUGIN_GUID = $"{PLUGIN_AUTHOR}.{PLUGIN_NAME}";
+        public const string PLUGIN_AUTHOR = "PluginAuthor";
+        public const string PLUGIN_NAME = "PluginName";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        private void Awake()
+        {
+            // Initialise le logger global du plugin
+            Log.Init(Logger);
+        }
+
+        private void Update()
+        {
+            var localPlayer = PlayerController.Local;
+
+            if (localPlayer != null && localPlayer.HasStateAuthority)
+            {
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    // On récupère au hasard un des sons de pets existants
+                    string text = "FART_" + Random.Range(1, 9);
+                    // On broadcast le son que l'on souhaite lancer
+                    GameManager.Rpc_BroadcastFollowSound(localPlayer.Runner, text, localPlayer.transform.position, 25);
+                    Log.Debug($"Playing sound {text}");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# LycansModTemplate
+
+Ce repo contiens un mod d'exemple pour Lycans.
+
+Vous pouvez cloner ce repo ou vous en servir comme d'une Template, ouvrir le fichier 
+solution dans **Visual Studio** et commencer à développer directement.
+
+Ce plugin d'exemple devrait vous permettre d'avoir tout ce dont vous avez besoin pour bien 
+commencer à développer un mod simple. Il setup les éléments suivants :
+
+- Un projet `.csproj` préconfigurer avec les paquets nécessaires
+- Un logger global
+- Un plugin basique qui broadcast un son
